3eec0dc1cdd1a3be1d435b4a809bc50e766f5675
anand kishore <anand@semanticvoid.com>
2009-02-27 15:56:18 -0800

    added textparser

---
+++ html/textparser.py
@@ -0,0 +1,25 @@
+from HTMLParser import HTMLParser
+
+class textparser(HTMLParser):
+
+	def __init__(self):
+		HTMLParser.__init__(self)
+		self.data = []
+		self.appendDataFlag = False
+
+	def handle_starttag(self, tag, attrs):
+		self.appendDataFlag = True
+
+	def handle_endtag(self, tag):
+		self.appendDataFlag = False
+	
+	def handle_data(self, data):
+		self.data.append(data)
+
+	def getData(self):
+		return ''.join(self.data)
+
+if __name__ == "__main__":
+	parser = textparser()
+	parser.feed("<html><body>this is a test</body></html>")
+	print "%s" % parser.getData()
