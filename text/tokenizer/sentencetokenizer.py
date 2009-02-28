1b423adb4d0c438a8563c47be4fbee6d2fe870a3
anand kishore <anand@semanticvoid.com>
2009-02-27 23:10:25 -0800

    extremely stupid sentence tokenizer

--- text/tokenizer/sentencetokenizer.py
+++ text/tokenizer/sentencetokenizer.py
@@ -1,10 +1,22 @@
+import re
+
+from tokenizer import Tokenizer
+
 """
-Class to tokenize paragraph into valid sentences
+Class to tokenize paragraphs into sentences
+TODO: base this class on the lines of StreamTokenizer.java
+[http://suif.stanford.edu/~mhn/deadlock/elevator/java/io/StreamTokenizer.java.html]
 """
 class SentenceTokenizer(Tokenizer):
 
-	def __init__(self):
+        def __init__(self):
 		Tokenizer.__init__(self)
 
 	def tokenize(self, data):
-		raise Exception('')
+		return re.split(r'[.:]+', data)
+
+if __name__ == '__main__':
+	# USAGE
+	t = SentenceTokenizer()
+	print t.tokenize('this is a test. Only a test [yeah right].')
+
